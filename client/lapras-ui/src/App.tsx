ed49a24f2dc00d7fc2ce70d722af6e56d15acc6a
Pablo Cha <pablocha1992@gmail.com>
2022-09-20 19:06:14 +0200

    feat(client): renew token

--- client/lapras-ui/src/App.tsx
+++ client/lapras-ui/src/App.tsx
@@ -2,13 +2,16 @@ import './App.css'
 import Products from "./products";
 import {Container} from "@mui/material";
 import Header from "./header";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import LoginForm from "./LoginForm";
 import {useCookies} from "react-cookie";
+import {ApiUrl} from "./constants";
+
+export default App
 
 function App() {
-    const [loggedIn, setLoggedIn] = useState(false)
     const [cookies, setCookies, removeCookies] = useCookies(['token'])
+    const [loggedIn, setLoggedIn] = useState(cookies.token != "")
     const [filterTerm, setFilterTerm] = useState("")
 
     const handleLoggedIn = () => {
@@ -18,17 +21,40 @@ function App() {
         removeCookies('token')
         setLoggedIn(false)
     }
+
     const setToken = (token:string) => setCookies('token', token);
+
+    const renewToken = () => {
+        fetch(ApiUrl + "/users/token/renew", {
+            method: 'POST',
+            headers: {
+                'Authorization': "Bearer " + cookies.token
+            }
+        }).then(res => {
+            switch (res.status) {
+                case 200:
+                    res.json()
+                        .then(body => setToken(body.token))
+                    break
+                case 401:
+                    logOut()
+            }
+        })
+    }
+
+    useEffect(() => {
+        if (loggedIn) {
+            renewToken();
+        }
+    })
 
     return (
         <div className="App" style={{overflow: "hidden"}}>
             <Header loggedIn={loggedIn} handleLogOut={() => setLoggedIn(false)} handleSearch={setFilterTerm}/>
             <LoginForm show={!loggedIn} handleLogIn={handleLoggedIn} setToken={setToken}/>
             {loggedIn ? <Container id="content" sx={{}}>
-                <Products apiURL={import.meta.env.API_URL} apiToken={cookies.token.toString()} logOut={logOut} filterTerm={filterTerm}/>
+                <Products apiURL={import.meta.env.API_URL} apiToken={cookies.token} logOut={logOut} filterTerm={filterTerm}/>
             </Container>: ""}
         </div>
     );
 }
-
-export default App
